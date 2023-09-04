ff4a84e6ad4b0d47acce4841cc73f9e65b99262a
Lenovo <Lenovo@Lenovo-PC>
2023-09-04 15:59:21 +0530

    Practice Programs file

---
+++ Prctice_Programs/src/com/practice/java/Add2Numbers.java
@@ -0,0 +1,12 @@
+package com.practice.java;
+
+public class Add2Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int a=10;
+  int b=20;
+  System.out.println("addition of two numbers is:" +(a+b));
+	}
+
+}
---
+++ Prctice_Programs/src/com/practice/java/demo.java
@@ -0,0 +1,16 @@
+package com.practice.java;
+
+public class demo {
+	public static void main(String[] args) {
+		int i=0;
+	while(i<=10) {
+	
+		if(i==3) 
+			break;
+		
+		System.out.println(i);
+		i++;
+	}
+	}
+
+}
